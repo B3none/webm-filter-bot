c1ff1b27ef5d97eb02d1c6aeaffc3e7d1685e075
B3none <ablackham2000@gmail.com>
2018-10-25 00:58:10 +0100

    fixed bot 2

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 const config = require("../config/config.json");
-const project = require('../package.json');
 
 const messageHelper = require('./helpers/message');
 
 client.on("ready", () => {
     console.log(`NSFW filter bot started.`);
-    client.user.setActivity(project.version);
 });
 
 client.on("message", message => messageHelper.message(client, message));
